2073a251dc3b3418f09912f418c51404231e0cf8
cristianvaz <cristianvaz81993@gmail.com>
2022-07-11 17:52:33 +0200

    sm8250-common: set selinux permissive for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,7 +114,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
-BOARD_KERNEL_CMDLINE := earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm kpti=off
+BOARD_KERNEL_CMDLINE := earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm kpti=off androidboot.selinux=permissive
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
